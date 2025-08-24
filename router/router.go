3511401e7d294dfd04ebb6161e690ef3a1ecc31c
luizfernando <lf.nandooliveeira@gmail.com>
2025-08-24 16:46:21 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -2,16 +2,15 @@ package router
 
 import "github.com/gin-gonic/gin"
 
-// Para ter uma "classe"/"funcao" publica, somente precisamos inicar a variavel com maiusculo 
+// Para ter uma "classe"/"funcao" publica, somente precisamos inicar a variavel com maiusculo
 
 func Initialize() {
-	// Inicializa o Router utilizando as configurações Default do gin
+	// Initialize Router
 	router := gin.Default()
-	// Defino uma rota em GO
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize Routes
+	initializeRoutes(router)
+
+	// Run the server
 	router.Run("localhost:8080")
 }
