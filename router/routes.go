0b88b89b506b9b382c785c893a949e0badc3c67f
luizfernando <lf.nandooliveeira@gmail.com>
2025-08-24 17:33:54 -0300

    feat: add custom logger

--- router/routes.go
+++ router/routes.go
@@ -9,15 +9,11 @@ func initializeRoutes(router *gin.Engine) {
 
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/oppening", handler.ShowOpeningHanlder())
-
-		v1.POST("/oppening", handler.CreateOpeningHanlder())
-
-		v1.DELETE("/oppening", handler.DeleteOpeningHanlder())
-
-		v1.PUT("/oppening", handler.UpdateOpeningHanlder())
-
-		v1.GET("/oppenings", handler.ListOpeningsHanlder())
+		v1.GET("/oppening", handler.ShowOpeningHanlder)
+		v1.POST("/oppening", handler.CreateOpeningHanlder)
+		v1.DELETE("/oppening", handler.DeleteOpeningHanlder)
+		v1.PUT("/oppening", handler.UpdateOpeningHanlder)
+		v1.GET("/oppenings", handler.ListOpeningsHanlder)
 
 	}
 
